061604ed7b4104a61a93a463596a43fdd78e6478
Ryan Holzapfel <sockhead13@gmail.com>
2014-03-22 20:49:32 -0400

    Added a Time Picker Dialog and Date Picker Dialog to MainActivity.java and updated the create_event_layout in order to utilize the pickers.

--- RemindMe/src/com/coop/remindme/MainActivity.java
+++ RemindMe/src/com/coop/remindme/MainActivity.java
@@ -1,20 +1,79 @@
 package com.coop.remindme;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
+	
+	private int year;
+	private int month;
+	private int day;
+	private int hour;
+	private int minute;
+	
+	static final int DATE_PICKER_ID = 1;
+	static final int TIME_PICKER_ID = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main_screen);
     }
+    
+    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+        	year = selectedYear;
+        	month = selectedMonth;
+        	day = selectedDay;
+        	((Button)findViewById(R.id.selectDate)).setText(month+"/"+day+"/"+year);
+        	
+        }
+    };
+    private TimePickerDialog.OnTimeSetListener pickerListener2 = new TimePickerDialog.OnTimeSetListener(){
+        public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+        	hour = selectedHour;
+        	minute = selectedMinute;
+        	((Button)findViewById(R.id.selectTime)).setText(hour+":"+minute);
+        }
+    };
+    
+    protected Dialog onCreateDialog(int id) {
+    	switch(id) {
+    	case DATE_PICKER_ID:
+    		return new DatePickerDialog(this, pickerListener, year, month, day);
+    	case TIME_PICKER_ID:
+    		return new TimePickerDialog(this, pickerListener2, hour, minute, false);
+    	}
+    	return null;
+    }
+    public void selectDate(View view) {
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+    	showDialog(DATE_PICKER_ID);
+    }
+    public void selectTime(View view) {
+    	final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        showDialog(TIME_PICKER_ID);
+    }
+
     
     public void createEvent(View view){
     	setContentView(R.layout.activity_create_event);
     }
 
+
 }
+
